6e8a2992a6ff9e1ed91d7977fe7524d6f3ed0767
lordstan1 <>
2022-09-21 01:01:55 +0300

    Add third exemplar.

--- src/main/java/hw/n2/N2.java
+++ src/main/java/hw/n2/N2.java
@@ -4,10 +4,13 @@ public class N2 {
 
     public static void main(String[] arg){
         Watch newWatchMechanical = new Watch(20, "mechanical");
-        Watch newWatchElectronic = new Watch(10, "electronic");
+        Watch newWatchElectronic = new Watch(25, "electronic");
+        Watch newWatchFashion = new Watch(16, "electronic");
         newWatchMechanical.checkCondition();
         newWatchMechanical.announceYears();
         newWatchElectronic.checkCondition();
         newWatchElectronic.announceYears();
+        newWatchFashion.checkCondition();
+        newWatchFashion.announceYears();
     }
 }
